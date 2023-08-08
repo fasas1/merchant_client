150dc4f022c1ce1492dc5e7bf00865277515f6bc
Fasasi Qasim <fasaqa1000@gmail.com>
2023-08-08 16:14:33 +0100

    initial commit

---
+++ src/Container/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from '../Components/Layout';
+
+
+
+
+function App() {
+  return (
+    <div>
+    <Header/>
+       Main componenet
+    </div>
+  );
+}
+
+export default App;
