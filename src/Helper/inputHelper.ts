8ada397153d5ae81aedaec7718c2536ac8ee187d
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-02-26 21:37:56 +0100

    Removed unnecessary import and prettier reformatted

--- src/Helper/inputHelper.ts
+++ src/Helper/inputHelper.ts
@@ -1,10 +1,13 @@
-import React, { ReactElement } from "react";
+import React from "react";
 
-const inputHelper = (e: React.ChangeEvent<HTMLInputElement |  HTMLSelectElement>, data:any) => {
-    const tempData: any = {...data};
-    tempData[e.target.name] = e.target.value
-    
-    return tempData;
-}
- 
+const inputHelper = (
+  e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  data: any
+) => {
+  const tempData: any = { ...data };
+  tempData[e.target.name] = e.target.value;
+
+  return tempData;
+};
+
 export default inputHelper;
