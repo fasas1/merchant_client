3daa2dbcf97b9f3e600a11a73e743be63ee6a07f
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-02-27 17:42:46 +0100

    No changes, Prettier reformat

--- src/Storage/Redux/productSlice.ts
+++ src/Storage/Redux/productSlice.ts
@@ -1,20 +1,19 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+import { productModel } from "../../Interfaces";
 
+const initialState: { product: productModel[] } = {
+  product: [],
+};
 
-const initialState = {
-      product:[],
-}
+export const productSlice = createSlice({
+  name: "Product",
+  initialState: initialState,
+  reducers: {
+    setProduct: (state, action) => {
+      state.product = action.payload;
+    },
+  },
+});
 
-export const  productSlice = createSlice({
-      name: "Product",
-      initialState:initialState,
-      reducers:{
-         setProduct: (state,action) =>{
-            state.product = action.payload
-         }
-      }
-})
-
-
-export const {setProduct} = productSlice.actions;
+export const { setProduct } = productSlice.actions;
 export const productReducer = productSlice.reducer;
