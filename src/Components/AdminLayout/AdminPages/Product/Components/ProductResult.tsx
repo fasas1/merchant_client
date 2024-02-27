bd2bdda557474e062b012134a28db687b786e324
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-02-27 17:40:44 +0100

    Feat: UI Refactor

--- src/Components/AdminLayout/AdminPages/Product/Components/ProductResult.tsx
+++ src/Components/AdminLayout/AdminPages/Product/Components/ProductResult.tsx
@@ -1,42 +1,110 @@
-import { Stack, Button, Grid } from "@mui/material";
+import {
+  Stack,
+  Button,
+  Grid,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Box from "@mui/system/Box";
 import styled from "@mui/system/styled";
 import { EditIcon, DeleteIcon } from "../../../../../Icon";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../../Storage/Redux/store";
+import { productModel } from "../../../../../Interfaces";
 
+type productForm = {
+  id: number;
+  name: string;
+  price: number;
+  category: Array<string>;
+};
 type Prop = {
   handleEditProduct: () => void;
   handleDeleteProduct: () => void;
+  productForm: productForm;
+  setProductForm: React.Dispatch<React.SetStateAction<productForm>>;
 };
-function ProductResult({ handleEditProduct, handleDeleteProduct }: Prop) {
+function ProductResult({
+  handleEditProduct,
+  handleDeleteProduct,
+  productForm,
+  setProductForm,
+}: Prop) {
+  const { product } = useSelector((state: RootState) => state.productReducer);
+  const Item = styled("div")(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "transparent",
+    borderColor: theme.palette.mode === "dark" ? "#444d58" : "#ced7e0",
+    padding: theme.spacing(1),
+    textAlign: "center",
+  }));
+
+  if (product.length === 0) {
+    return (
+      <Typography
+        variant='h4'
+        align='center'
+        component='h2'
+        sx={{ margin: "2rem 0" }}
+      >
+        No products available, Add a product
+      </Typography>
+    );
+  }
   return (
     <div>
       <header style={{ margin: "2rem", width: "100%" }}>
-        <Stack
-          direction='row'
-          alignItems='center'
-          justifyContent='space-between'
-          spacing={1}
-          useFlexGap
-          sx={{
-            display: { xs: "none", sm: "flex" },
-          }}
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: { xs: "none", sm: "flex" } }}
         >
-          <span style={{ marginLeft: "-2rem" }}>Name</span>
-          <span>Price</span>
-          <span>Category</span>
-          <span style={{ flexBasis: "38%" }}>Date</span>
-          <span style={{ textAlign: "center", flexBasis: "10%" }}>Action</span>
-        </Stack>
+          <Grid sm={2}>
+            <Item>
+              <Typography overflow='hidden' align='left'>
+                Name
+              </Typography>
+            </Item>
+          </Grid>
+          <Grid sm={2}>
+            <Item>
+              <Typography overflow='hidden' align='left'>
+                Price
+              </Typography>
+            </Item>
+          </Grid>
+          <Grid sm={2}>
+            <Item>
+              <Typography overflow='hidden' align='left'>
+                Category
+              </Typography>
+            </Item>
+          </Grid>
+          <Grid sm={3}>
+            <Item>
+              <Typography overflow='hidden' align='left'>
+                Date
+              </Typography>
+            </Item>
+          </Grid>
+          <Grid sm={3}>
+            <Item>
+              <Typography overflow='hidden'>Action</Typography>
+            </Item>
+          </Grid>
+        </Grid>
       </header>
 
       <main>
-        {Array.from(Array(6)).map((product, index) => {
+        {product.map((product) => {
           return (
             <ProductItem
+              product={product}
               handleEditProduct={handleEditProduct}
               handleDeleteProduct={handleDeleteProduct}
-              key={index}
-              index={index}
+              key={product.id}
+              productForm={productForm}
+              setProductForm={setProductForm}
             />
           );
         })}
@@ -47,94 +115,140 @@ function ProductResult({ handleEditProduct, handleDeleteProduct }: Prop) {
 export default ProductResult;
 
 type ProductItemProp = {
-  index: number;
+  product: productModel;
   handleEditProduct: () => void;
   handleDeleteProduct: () => void;
+  productForm: productForm;
+  setProductForm: React.Dispatch<React.SetStateAction<productForm>>;
 };
 
 const ProductItem = ({
-  index,
+  product,
   handleEditProduct,
   handleDeleteProduct,
+  productForm,
+  setProductForm,
 }: ProductItemProp) => {
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("sm"));
   const Item = styled("div")(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    border: "1px solid",
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "transparent",
+    border: isMatch ? "1px solid" : "0px solid",
     borderColor: theme.palette.mode === "dark" ? "#444d58" : "#ced7e0",
     padding: theme.spacing(1),
-    borderRadius: "4px",
+    borderRadius: isMatch ? "4px" : "0px",
     textAlign: "center",
   }));
+  const { id, name, price, category } = product;
   return (
     <div>
-      <Stack
-        direction='row'
-        alignItems='center'
-        justifyContent='space-between'
-        spacing={1}
-        useFlexGap
-        sx={{
-          display: {
-            xs: "none",
-            sm: "flex",
-            textAlign: "center",
-            borderBottom: "1px solid #112121",
-            margin: "1rem 0",
-            padding: ".5rem 0",
-          },
-        }}
-      >
-        <span>Product {index}</span>
-        <span>Price {index}</span>
-        <span>Category {index}</span>
-        <span style={{ flexBasis: "40%", textAlign: "left" }}>
-          Date {index}
-        </span>
-        <Box sx={{ display: "flex", justifyContent: "flex-end", gap: "1rem" }}>
-          <Button onClick={handleEditProduct}>
-            <EditIcon />
-          </Button>
-          <Button onClick={handleDeleteProduct}>
-            <DeleteIcon />
-          </Button>
-        </Box>
-      </Stack>
-      {/* Mobile Screen */}
       <Box
         sx={{
           flexGrow: 1,
-          display: { xs: "flex", sm: "none" },
           margin: "2rem 0",
         }}
       >
         <Grid container spacing={2}>
-          <Grid xs={8}>
-            <Item>Product {index}</Item>
+          <Grid xs={8} sm={2}>
+            <Item>
+              <Stack
+                direction='column'
+                alignItems='center'
+                justifyContent='space-between'
+                spacing={1}
+                useFlexGap
+              >
+                <Typography sx={{ display: { xs: "flex", sm: "none" } }}>
+                  Name:
+                </Typography>
+                <Typography overflow='clip'>{name}</Typography>
+              </Stack>
+            </Item>
           </Grid>
-          <Grid xs={4}>
-            <Item>Price {index}</Item>
+          <Grid xs={4} sm={2}>
+            <Item>
+              <Stack
+                direction='column'
+                alignItems='center'
+                justifyContent='space-between'
+                spacing={1}
+                useFlexGap
+              >
+                <Typography sx={{ display: { xs: "flex", sm: "none" } }}>
+                  Price:
+                </Typography>
+                <Typography overflow='clip'>&#8358;{price}</Typography>
+              </Stack>
+            </Item>
           </Grid>
-          <Grid xs={6}>
-            <Item>Category {index}</Item>
+          <Grid xs={6} sm={2}>
+            <Item>
+              <Stack
+                direction='column'
+                alignItems='center'
+                justifyContent='space-between'
+                spacing={1}
+                useFlexGap
+              >
+                <Typography sx={{ display: { xs: "flex", sm: "none" } }}>
+                  Category:
+                </Typography>
+                <Typography overflow='clip'>{category}</Typography>
+              </Stack>
+            </Item>
           </Grid>
-          <Grid xs={6}>
-            <Item>Date {index}</Item>
+          <Grid xs={6} sm={3}>
+            <Item>
+              <Stack
+                direction='column'
+                alignItems='center'
+                justifyContent='space-between'
+                spacing={1}
+                useFlexGap
+              >
+                <Typography sx={{ display: { xs: "flex", sm: "none" } }}>
+                  Date:
+                </Typography>
+                <Typography overflow='clip'>TBD</Typography>
+              </Stack>
+            </Item>
           </Grid>
 
-          <Grid xs={12}>
-            <Item
-              sx={{ display: "flex", justifyContent: "flex-end", gap: "1rem" }}
-            >
-              <Button variant='contained' onClick={handleEditProduct}>
-                Edit
-              </Button>
-              <Button
-                variant='contained'
-                color='error'
-                onClick={handleDeleteProduct}
+          <Grid xs={12} sm={3}>
+            <Item>
+              <Box
+                sx={{
+                  display: { xs: "flex" },
+                  justifyContent: "end",
+                  gap: isMatch ? "1rem" : "0",
+                }}
               >
-                Delete
-              </Button>
+                <Button
+                  variant={isMatch ? "contained" : "text"}
+                  onClick={() => {
+                    setProductForm({
+                      ...productForm,
+                      id,
+                      name,
+                      price,
+                      category: [
+                        category,
+                        ...productForm.category.filter((c) => c !== category),
+                      ],
+                    });
+                    handleEditProduct();
+                  }}
+                >
+                  {isMatch ? <Typography>Edit</Typography> : <EditIcon />}
+                </Button>
+                <Button
+                  variant={isMatch ? "contained" : "text"}
+                  color={isMatch ? "error" : "inherit"}
+                  onClick={handleDeleteProduct}
+                >
+                  {isMatch ? <Typography>Delete</Typography> : <DeleteIcon />}
+                </Button>
+              </Box>
             </Item>
           </Grid>
         </Grid>
