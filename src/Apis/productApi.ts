534fe496aebb82cf22227a89fe1a65643bf1e453
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-02-27 17:39:31 +0100

    Added Fetch function to add, edit and delete products

--- src/Apis/productApi.ts
+++ src/Apis/productApi.ts
@@ -21,8 +21,46 @@ const productApi = createApi({
       }),
       providesTags: ["Products"],
     }),
+    addProduct: builder.query({
+      query: (product) => ({
+        url: "product",
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: product,
+      }),
+      providesTags: ["Products"],
+    }),
+    editProduct: builder.query({
+      query: (product) => ({
+        url: `product/${product.id}`,
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: product,
+      }),
+      providesTags: ["Products"],
+    }),
+    deleteProduct: builder.query({
+      query: (id) => ({
+        url: `product/${id}`,
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+        },
+      }),
+      providesTags: ["Products"],
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetProductByIdQuery } = productApi;
+export const {
+  useGetProductsQuery,
+  useGetProductByIdQuery,
+  useAddProductQuery,
+  useEditProductQuery,
+  useDeleteProductQuery,
+} = productApi;
 export default productApi;
