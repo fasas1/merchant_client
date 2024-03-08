f826db882dd049dbeecbe15c6f53cf1e9ca5fc4e
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-08 18:15:41 +0100

    Added chevron right icon

--- src/Icon/index.tsx
+++ src/Icon/index.tsx
@@ -86,3 +86,23 @@ export function BackIcon({ h, w }: Prop) {
     </svg>
   );
 }
+
+export function RightArrowIcon({ h, w }: Prop) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={3.5}
+      stroke='#fff'
+      width={h ? h : 15}
+      height={w ? w : 15}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='m8.25 4.5 7.5 7.5-7.5 7.5'
+      />
+    </svg>
+  );
+}
